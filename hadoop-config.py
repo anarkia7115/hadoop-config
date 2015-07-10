fff7520c0ce1be744120f946c4971ed309f393cc
anarkia7115 <marc7115@gmail.com>
2015-07-10 08:07:24 +0800

    add py

---
+++ hadoop-config.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+
+import sys;
+
+def print_opts(m, c):
+	m_j = int(0.8 * m)
+	r_j = int(2 * m_j)
+	r = 2 * m
+	s_min = m
+	s_max = m * c
+	n_res = m * c
+
+	print  \
+	"  --mapred-key-value mapreduce.map.java.opts=-Xmx{}m".format(m_j), "\n", \
+	"  --mapred-key-value mapreduce.reduce.java.opts=-Xmx{}m".format(r_j), "\n", \
+	"  --mapred-key-value mapreduce.map.memory.mb={}".format(m), "\n", \
+	"  --mapred-key-value mapreduce.reduce.memory.mb={}".format(r), "\n", \
+	"  --yarn-key-value yarn.scheduler.minimum-allocation-mb={}".format(s_min), "\n", \
+	"  --yarn-key-value yarn.scheduler.maximum-allocation-mb={}".format(s_max), "\n", \
+	"  --yarn-key-value yarn.nodemanager.resource.memory-mb={}".format(n_res)
+
+
+def main(argv):
+	m = 1440
+	c = 8
+
+	if len(argv) == 1:
+		# no arguments
+		# do nothing
+		pass
+	elif len(argv) == 2:
+		# has map
+		m = argv[1]
+		c = 1
+	elif len(argv) == 3:
+		# has m and c
+		m = argv[1]
+		c = argv[2]
+	
+	print_opts(m, c)
+
+
+
+if __name__ == "__main__":
+	main(sys.argv)
